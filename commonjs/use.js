fea62e05b51ba9dd60a01ce6aeb6242df39f52b6
jason-xiewenqiang <2780535515@qq.com>
2024-01-10 19:33:08 +0800

    feat: use umd

--- commonjs/use.js
+++ commonjs/use.js
@@ -1,5 +1,8 @@
 const clone = require("./define.js");
+const clone1 = require('../umd/clone.umd.js');
 const a = { a: 1 };
 const b = clone(a);
+const c = clone1(a);
 
 console.log(a === b);
+console.log(a === c);
