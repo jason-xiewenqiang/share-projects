84b8cfe55e412a955853b022d5974b01509a45d3
jason-xiewenqiang <2780535515@qq.com>
2024-01-30 11:18:31 +0800

    feat: 完善rollup 例子

--- rollup-entry/rollup.config.js
+++ rollup-entry/rollup.config.js
@@ -1,11 +1,36 @@
 import { nodeResolve as resolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
+import babel from '@rollup/plugin-babel';
+import pkg  from './package.json' assert { type: "json" };
+import clear from 'rollup-plugin-clear';
+
+console.log(`\n${pkg.name}-${pkg.version}\n`);
 
 export default {
-  entry: 'src/index.js',
-  output: [],
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/inde-cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'Entry',
+      plugins: [terser()]
+    },
+    {
+      file: 'dist/index-iife.js',
+      format: 'iife',
+    },
+  ],
   plugins: [
+    clear({ targets: ['dist'] }),
+    json(),
     resolve(),
     commonjs(),
+    babel({ babelHelpers: 'bundled' })
   ]
 }
